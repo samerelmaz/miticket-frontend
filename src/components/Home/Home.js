44e1ef357dce732eb555e63fd62127c335db0b00
samerelmaz <samerelmaz@gmail.com>
2020-07-11 10:34:42 -0400

    Agregar boton de cerrar sesion

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './Home.css';
 import { connect } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
 import AdminView from '../AdminView/AdminView';
 import UserView from '../UserView/UserView';
+import { Button } from 'antd';
 
 function Home(props) {
     return (
         <>
             {!props.user.id && <Redirect to='/login' />}
+            <Link to='/'><Button type='secondary' className='logout-btn' >Cerrar sesión</Button></Link>
             <h1 className='center-text mt-3'>Bienvenido, {props.user.name}</h1>
             {props.user.userType === 1 ? <UserView /> : <AdminView />}             
         </>
